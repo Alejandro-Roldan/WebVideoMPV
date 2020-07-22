0e43565718738d07804d145ad1a58e6f6c9234f2
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2020-07-22 15:10:29 +0200

    Add files via upload

--- WebVideoMPV.sh
+++ WebVideoMPV.sh
@@ -4,12 +4,13 @@ link=$(xclip -o -selection clipboard)
 
 played_file=$HOME/.playedlinks
 
-if mpv "$link"; then
-	echo "$link" >> "$played_file"
-else
+echo "$link" >> "$played_file"
+
+if ! mpv "$link"; then
+	sed -i '$d' "$played_file"
 	old_link=$(tail -n 1 "$played_file")
 	mpv "$old_link"
 fi
 
 ## One liner
-# link=$(xclip -o -selection clipboard); played_file=$HOME/.playedlinks; mpv "$link" && echo "$link" >> "$played_file" || old_link=$(tail -n 1 "$played_file"); mpv "$old_link"
+# link=$(xclip -o -selection clipboard); played_file=$HOME/.playedlinks; echo "$link" >> "$played_file"; mpv "$link" || sed -i '$d' "$played_file"; old_link=$(tail -n 1 "$played_file"); mpv "$old_link"
