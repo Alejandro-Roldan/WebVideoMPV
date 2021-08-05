d08bd60697864d740cf6dded2bd804607f1dbc3c
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2021-08-05 20:04:20 +0200

    Add files via upload

--- WebVideoMPV.sh
+++ WebVideoMPV.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+## A SCRIPT TO PLAY CLIPBOARDED URLS WITH MPV
+
+# Get cliboard content
 link=$(xclip -o -selection clipboard)
 
-played_file=$HOME/.playedlinks
+# Directory for a history of played urls
+history=$HOME/.webvideompvhistory
 
-echo "$link" >> "$played_file"
+# Save clipboard to the history
+# Do this before playing so if it crashes you still have the history
+echo "$link" >> "$history"
 
+# Try to play the clipboard with mpv, and if it fails
 if ! mpv "$link"; then
-	sed -i '$d' "$played_file"
-	old_link=$(tail -n 1 "$played_file")
+	# Remove the last added line from history
+	sed -i '$d' "$history"
+	# Get the last item from the history
+	old_link=$(tail -n 1 "$history")
+	# Play that instead
 	mpv "$old_link"
 fi
 
 ## One liner
-# link=$(xclip -o -selection clipboard); played_file=$HOME/.playedlinks; echo "$link" >> "$played_file"; mpv "$link" || sed -i '$d' "$played_file"; old_link=$(tail -n 1 "$played_file"); mpv "$old_link"
+# link=$(xclip -o -selection clipboard); history=$HOME/.webvideompvhistory; echo "$link" >> "$history"; mpv "$link" || sed -i '$d' "$history"; old_link=$(tail -n 1 "$history"); mpv "$old_link"
